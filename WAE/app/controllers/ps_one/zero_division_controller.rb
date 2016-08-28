3e10856e8ee15fa0b209cc4ad120161b458f061e
Subigya Jyoti Panta <subigyapanta@gmail.com>
2016-08-28 16:48:15 +0700

    1. Reorganised files for youtube contoller, helper, javascript 2. Error Logging is done in Zero Division 3. Css rule is added for resources.

--- WAE/app/controllers/ps_one/zero_division_controller.rb
+++ WAE/app/controllers/ps_one/zero_division_controller.rb
@@ -1,4 +1,11 @@
 class PsOne::ZeroDivisionController < ApplicationController
   def index
+    @divideByZero = "No Divide by Zero.";
+    begin
+      logger.debug "About to divide by zero"
+      @hello = 1/0;
+    rescue ZeroDivisionError
+      @divideByZero = "Divide by Zero Exception ocured";
+    end
   end
 end
---
+++ WAE/app/helpers/ps_one/youtube_helper.rb
@@ -0,0 +1,2 @@
+module PsOne::YoutubeHelper
+end
