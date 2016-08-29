5356075f8e5208d17abf044d38b8fce4a8b8af42
Subigya Jyoti Panta <subigyapanta@gmail.com>
2016-08-29 19:09:36 +0700

    separated fetching logic to controller

--- WAE/app/controllers/ps_one/youtube_controller.rb
+++ WAE/app/controllers/ps_one/youtube_controller.rb
@@ -3,7 +3,7 @@ class PsOne::YoutubeController < ApplicationController
     #require 'rubygems'
     #require 'nokogiri'
     require 'open-uri'
-    @page = Nokogiri::HTML(open('https://www.youtube.com/feed/trending'),nil,"UTF-8")
-
+    page = Nokogiri::HTML(open('https://www.youtube.com/feed/trending'),nil,"UTF-8")
+    @trending = page.css('a.yt-uix-sessionlink.yt-uix-tile-link.yt-ui-ellipsis.yt-ui-ellipsis-2.spf-link').take(10);
   end
 end
