f64283c4594f13ccf615021d868d7cfda7c8aa2d
Subigya Jyoti Panta <subigyapanta@gmail.com>
2016-10-12 01:36:58 +0700

    User Login BDD done

--- WAE/features/UserLoginRegister.feature
+++ WAE/features/UserLoginRegister.feature
@@ -5,16 +5,39 @@ Feature: User Registration
 
     Given I visit the user registration page
     And I submit my details with ait asia email
-    Then Check that i am logged in with ait asia email
+    Then Check that i am registered with ait asia email
     And Logout
 
     Given I visit the user registration page
     And I submit my details with ait ac email
-    Then Check that i am logged in with ait ac email
+    Then Check that i am registered with ait ac email
+    And Logout
+
 
   Scenario: I want to register myself with other email address
+
     Given I visit the user registration page
     And I submit my details with other email
-    Then I should see an error
+    Then I should see "Email is invalid" error
+
+
+  Scenario: I want to login using Ait email address
+
+    Given I visit user login page
+    And I submit my ait asia login credentials
+    Then Check that i am logged in with ait asia email
+    And Logout
+
+    Given I visit user login page
+    And I submit my ait ac login credentials
+    Then Check that i am logged in with ait ac email
+    And Logout
+
+
+  Scenario: I want to login using other email address
+
+    Given I visit user login page
+    And I submit my other email address
+    Then I should stay in the same page unable to login
 
 
--- WAE/features/step_definitions/user_login_registration.rb
+++ WAE/features/step_definitions/user_login_registration.rb
@@ -10,7 +10,7 @@ Given(/^I submit my details with ait asia email$/) do
   click_button "Sign up"
 end
 
-Then(/^Check that i am logged in with ait asia email$/) do
+Then(/^Check that i am registered with ait asia email$/) do
   visit ps_four_path
   expect(page).to have_content("Logged in as: " + @ait_asia.email)
 end
@@ -28,8 +28,9 @@ Given(/^I submit my details with ait ac email$/) do
   click_button "Sign up"
 end
 
-Then(/^Check that i am logged in with ait ac email$/) do
+Then(/^Check that i am registered with ait ac email$/) do
   visit ps_four_path
+  # save_and_open_page
   expect(page).to have_content("Logged in as: " + @ait_ac.email)
 end
 
@@ -41,8 +42,56 @@ Given(/^I submit my details with other email$/) do
   click_button "Sign up"
 end
 
-Then(/^I should see an error$/) do
-  expect(page).to have_content("Email is invalid")
+Then(/^I should see "([^"]*)" error$/) do |arg1|
+  expect(page).to have_content(arg1)
 end
+
+Given(/^I visit user login page$/) do
+  visit new_user_session_path
+end
+
+Given(/^I submit my ait asia login credentials$/) do
+  @ait_asia = FactoryGirl.create :NewAitAsiaUser
+  fill_in "Email", with: @ait_asia.email
+  fill_in "Password", with: @ait_asia.password
+  click_button "Log in"
+end
+
+Then(/^Check that i am logged in with ait asia email$/) do
+  visit ps_four_path
+  expect(page).to have_content("Logged in as: " + @ait_asia.email)
+end
+
+Given(/^I submit my ait ac login credentials$/) do
+  @ait_ac = FactoryGirl.create :NewAitAcUser
+  fill_in "Email", with: @ait_ac.email
+  fill_in "Password", with: @ait_ac.password
+  click_button "Log in"
+end
+
+
+Then(/^Check that i am logged in with ait ac email$/) do
+  visit ps_four_path
+  # save_and_open_page
+  expect(page).to have_content("Logged in as: " + @ait_ac.email)
+end
+
+Given(/^I submit my other email address$/) do
+  @other = FactoryGirl.build :NewGmailUser
+  fill_in "Email", with: @other.email
+  fill_in "Password", with: @other.password
+  click_button "Log in"
+end
+
+Then(/^I should stay in the same page unable to login$/) do
+  expect(page).to have_content("Email")
+  expect(page).to have_content("Password")
+  expect(page).to have_content("Log in")
+  # save_and_open_page
+
+  expect(page).to have_field("Email", :with => @other.email)
+end
+
+
 
 
