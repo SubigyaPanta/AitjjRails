b44ef6246abd9f661385e63fed48f7a42c46b99f
Subigya Jyoti Panta <subigyapanta@gmail.com>
2016-10-14 17:01:26 +0700

    Capistrano deploy:seed is working

--- WAE/config/deploy.rb
+++ WAE/config/deploy.rb
@@ -56,24 +56,24 @@ set :rbenv_ruby, '2.3.1'
 
 # Add this in config/deploy.rb
 # and run 'cap production deploy:seed' to seed your database
-# namespace :deploy do
-#   desc 'Runs rake db:seed'
-#   task :seed => [:set_rails_env] do
-#     on primary fetch(:migration_role) do
-#       within release_path do
-#         with rails_env: fetch(:rails_env) do
-#           execute :rake, "db:seed"
-#         end
-#       end
-#     end
-#   end
-# end
-set :rails_env, fetch(:default_stage)
-
 namespace :deploy do
   desc 'Runs rake db:seed'
-  task :seed do
-    # execute :rake, "db:seed"
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
   end
 end
+# set :rails_env, fetch(:default_stage)
+#
+# namespace :deploy do
+#   desc 'Runs rake db:seed'
+#   task :seed do
+#     # execute :rake, "db:seed"
+#     run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+#   end
+# end
--- WAE/lib/capistrano/tasks/seed.rake
+++ WAE/lib/capistrano/tasks/seed.rake
@@ -1,7 +1,7 @@
 #to run db:seed after deployment
-namespace :deploy do
-  desc "reload the database with seed data"
-  task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
-  end
-end
+# namespace :deploy do
+#   desc "reload the database with seed data"
+#   task :seed do
+#     run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+#   end
+# end
