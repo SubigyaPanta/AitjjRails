2dbd316b3a41df3710cfb926b46f2619d9765f66
Subigya Jyoti Panta <subigyapanta@gmail.com>
2016-11-15 00:28:55 +0700

    Validation of comments and its message to user is provided properly

--- WAE/app/assets/javascripts/products.js
+++ WAE/app/assets/javascripts/products.js
@@ -1,15 +1,39 @@
 (function($){
     $(document).on('ready turbolinks:load', function () {
         var requestRunning = false;
+
+        function showMessage(messageBoxId, messageText, containerClass){
+            var msg = $(document.createElement('div'));
+            var container = $('#alert-container-'+messageBoxId);
+            msg.attr({class: containerClass, role: 'alert'});
+            msg.text(messageText);
+
+            // clear it incase it not empty
+            container.empty();
+            container.append(msg);
+
+            // make it fadeout and disappear after 5 seconds
+            setTimeout(function () {
+                container.fadeOut('slow', function () {
+                    container.empty();
+                })
+            }, 5000)
+        }
+
         $('.comment').on('click', function(){
+
+            var id = $(this).data('product');
+            var comment = $('#product-'+id).val();
+            // Return if comment is empty. This should be above requestRunning otherwise, requestRunning will be
+            // true and will exit continuously. (It is set to false on complete callback.
+            if (comment.trim() == ''){
+                showMessage(id, 'Comment box is empty.', 'alert alert-danger');
+                return;
+            }
             if( requestRunning ){
                 return;
             }
             requestRunning = true;
-            var id = $(this).data('product')
-            var comment = $('#product-'+id).val();
-            var container = $('#alert-container-'+id)
-            console.log(comment);
 
 
             $.ajax({
@@ -24,18 +48,14 @@
                 },
                 success: function (response, textStatus, jqXHR) {
                     //<div class="alert alert-success" role="alert">...</div>
-                    var alert = $(document.createElement('div')).attr({class: 'alert alert-success', role: 'alert'});
-                    alert.text('Comment posted.');
                     console.log(response, textStatus, jqXHR);
-                    container.empty();
-                    container.append(alert);
+                    showMessage(id, 'Comment Posted.', 'alert alert-success');
                 },
                 error: function (jqXhr, textStatus, errorThrown) {
-                    console.log(jqXhr, textStatus, errorThrown)
-                    var alert = $(document.createElement('div')).attr({class: 'alert alert-danger', role: 'alert'});
-                    alert.text('Could not post comment');
-                    container.empty();
-                    container.append(alert);
+                    console.log(jqXhr, textStatus, errorThrown);
+                    showMessage(id, 'Could not post comment', 'alert alert-danger');
+                    // msg.attr({class: 'alert alert-danger', role: 'alert'});
+                    // msg.text('Could not post comment');
                 },
                 complete: function (jqXhr, textStatus) {
                     $('#product-'+id).val('');
--- WAE/app/controllers/comments_controller.rb
+++ WAE/app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     user = current_user
     if !product.nil? && !user.nil?
       @comment = Comment.new :content => params[:comment][:content]
-      @comment.product = product
+      # @comment.product = product
       @comment.user = user
 
       respond_to do |format|
@@ -16,11 +16,11 @@ class CommentsController < ApplicationController
           format.json { render :create, status: :created }
         else
           # format.html { render :new }
-          format.json { render json: @product.errors, status: :unprocessable_entity }
+          format.json { render json: @comment.errors, status: :unprocessable_entity }
         end
       end
     else
-      format.json { render json: @product.errors, status: :unprocessable_entity }
+      format.json { render json: @comment.errors, status: :unprocessable_entity }
     end
   end
 
