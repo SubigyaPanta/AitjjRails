49b9c7476c880bcd5750c7e21d1a2fe2070eece8
Subigya Jyoti Panta <subigyapanta@gmail.com>
2016-12-04 13:28:06 +0700

    Added helper for tooltip

--- WAE/app/helpers/products_helper.rb
+++ WAE/app/helpers/products_helper.rb
@@ -6,4 +6,12 @@ module ProductsHelper
   def search_product_category
     session[:product_category].nil? ? '' : session[:product_category]
   end
+
+  def show_tooltip_on_not_logged_in(tip, direction = 'top')
+    tooltip = ''
+    unless (defined?(user_signed_in)).nil? && user_signed_in?
+      tooltip = "data-toggle=\"tooltip\" data-placement=\"#{direction}\" title=\"#{tip}\""
+    end
+    tooltip.html_safe
+  end
 end
